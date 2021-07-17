4086c8d813a29dbd866f626d32cd16d07f915fc6
Djennet85 <71813188+Djennet85@users.noreply.github.com>
2021-07-17 18:04:08 +0300

    Update exer_3.py

--- exer_3.py
+++ exer_3.py
@@ -1,7 +1,8 @@
 # Задание №3. Узнать у пользователя число n.
 # Найти сумму чисел n+nn+nnn. Пример: 3+33+333=369
-import random
-n = int(input('Введите число : '))
-total = (n + int(str(n) + str(n)) + int(str(n) + str (n) + str (n)))
-print("Сумма чисел n + nn + nnn = %d" % total)
+n = input('Введите целое число : ')
+while n < "0":
+    print("Введите число больше 0!: ")
+    n = input('Введите целое число: ')
+print(f"{n} + {n + n} + {n + n + n} = {int(n) + int(n + n) + int (n + n + n)}")
 
